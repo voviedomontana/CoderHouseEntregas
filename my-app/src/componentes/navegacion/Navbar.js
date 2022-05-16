a8e5eb16c84b88eda88e784fc20acc8ea6fb6133
Valentino Oviedo <valenoviedo2001@gmail.com>
2022-05-16 20:19:55 -0300

    black navbar

--- my-app/src/componentes/navegacion/Navbar.js
+++ my-app/src/componentes/navegacion/Navbar.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 const Navbar = () => {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-black">
             <div className="container-fluid">
                 <Link to='/'>
                     <img src='./coderhouse-logo.png' width='70'/>
