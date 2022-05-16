76d623682c5deef88d588c9ec081c429710ce89d
Valentino Oviedo <valenoviedo2001@gmail.com>
2022-05-16 19:59:29 -0300

    Navbar React/Bootstrap

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router,Route, Routes} from 'react-router-dom'
+import Navbar from './componentes/navegacion/Navbar';
+import Experiences from './componentes/paginas/Experiences';
+import Home from './componentes/paginas/Home';
+import Proyects from './componentes/paginas/Proyects';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' exact component={Home}/>
+          <Route path='/Proyects' component={Proyects}/>
+          <Route path='/Experiences' component={Experiences}/>
+        </Routes>
+      </Router>
+      <h2>Portfolio React Valentino Oviedo</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/componentes/paginas/Proyects.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Proyects = () => {
+  return (
+    <div>
+        <h1>This are my proyects</h1>
+    </div>
+  )
+}
+
+export default Proyects
